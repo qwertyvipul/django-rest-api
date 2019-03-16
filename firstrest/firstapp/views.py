d9cad6fc48d147878b621b770b4e3d60b313b614
Vipul Sharma <vipuls526@gmail.com>
2019-03-17 01:37:31 +0530

    scripts

--- firstrest/firstapp/views.py
+++ firstrest/firstapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .scripts import hello
+
 # Create your views here.
 def printHello(request):
     if request.method == "GET":
         pass
 
-    return HttpResponse('<h1>Hello World!</h1>')
+    return HttpResponse(hello.printHello())
+    # return HttpResponse('<h1>Hello World!</h1>')
